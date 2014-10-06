aba449d58b84996ddd14c6fa6df86e99f27e705d
Bjørn Friese <bearfrieze@gmail.com>
2014-10-06 10:35:40 +0200

    Initial commit

---
+++ client.js
@@ -0,0 +1,114 @@
+// Config and stuffs
+var svgns = "http://www.w3.org/2000/svg",
+	scale = 10,
+	colors = ['white', 'red', 'green', 'blue', 'orange'],
+	moves = {left: [-1, 0], up: [0, -1], right: [1, 0], down: [0, 1]};
+
+// Set up WebSocket and listeners
+var ws = new WebSocket("ws://localhost:8080"),
+	game = null;
+ws.addEventListener('open', function() {
+	ws.send(JSON.stringify({
+		type: 'connect',
+		id: 'yellowWorld'
+	}));
+});
+ws.addEventListener('message', function(event) {
+	var message = JSON.parse(event.data);
+	if (message.type == 'newgame') {
+		game = new Game(message);
+		game.timer();
+	} else if (message.type == 'move') {
+		game.input(message);
+	} else if (message.type == 'gameover') {
+		clearInterval(game.interval);
+	} else if (message.type == 'reset') {
+		game.reset(message);
+	}
+});
+
+// Game
+var Game = function(message) {
+	this.initialize(message);
+}
+Game.prototype.initialize = function(message) {
+
+	this.id = message.id;
+	this.board = message.board;
+	this.cols = this.board[0].length;
+	this.rows = this.board.length;
+
+	this.svg = document.createElementNS(svgns, 'svg');
+	this.svg.setAttribute('width', this.cols * scale);
+	this.svg.setAttribute('height', this.rows * scale);
+	this.svg.setAttribute('style', 'border: 1px solid black;');
+	this.rects = [];
+	for (var y = 0; y < this.rows; y++) {
+		this.rects[y] = [];
+		for (var x = 0; x < this.cols; x++) {
+			var rect = document.createElementNS(svgns, 'rect');
+			rect.setAttribute('width', scale);
+			rect.setAttribute('height', scale);
+			rect.setAttribute('x', x * scale);
+			rect.setAttribute('y', y * scale);
+			this.rects[y][x] = rect;
+			this.svg.appendChild(rect);
+		}
+	}
+	document.body.appendChild(this.svg);
+	this.reset(message);
+
+	var that = this;
+	document.onkeydown = function(e) {
+		switch (e.keyCode) {
+			case 37: that.direction = moves['left']; break;
+			case 38: that.direction = moves['up']; break;
+			case 39: that.direction = moves['right']; break;
+			case 40: that.direction = moves['down']; break;
+			default: return;
+		}
+		e.preventDefault();
+	};
+};
+Game.prototype.reset = function(message) {
+	this.board = message.board;
+	this.location = {
+		x: Math.floor(Math.random() * this.cols),
+		y: Math.floor(Math.random() * this.rows)
+	};
+	this.direction = moves['right'];
+	this.render();
+	this.timer();
+};
+Game.prototype.renderSingle = function(x, y) {
+	var color = colors[this.board[y][x]];
+	this.rects[y][x].setAttribute('style', 'fill:' + color + ';');
+}
+Game.prototype.render = function() {
+	for (var y = 0; y < this.rows; y++) {
+		for (var x = 0; x < this.cols; x++) {
+			this.renderSingle(x, y);
+		}
+	}
+};
+Game.prototype.input = function(m) {
+	this.board[m.y][m.x] = m.id;
+	this.renderSingle(m.x, m.y);
+};
+Game.prototype.move = function() {
+	ws.send(JSON.stringify({
+		type: 'move',
+		id: this.id,
+		x: this.location.x += this.direction[0],
+		y: this.location.y += this.direction[1]
+	}));
+};
+Game.prototype.timer = function() {
+	clearInterval(this.interval);
+	this.interval = setInterval(function() {
+		if (ws.readyState == 1)
+			this.move();
+		else
+			clearInterval(this.interval);
+	}.bind(this), 100);
+};
---
+++ server.js
@@ -0,0 +1,90 @@
+// Set up server
+var WebSocketServer = require('ws').Server,
+	wss = new WebSocketServer({port: 8080}),
+	games = {};
+
+// Listen for new players and put them into games
+wss.on('connection', function(ws) {
+	console.log('Client connected');
+	ws.on('message', function(json) {
+		var message = JSON.parse(json);
+		if (message.type == 'connect') {
+			var id = message.id;
+			if (!games[id]) games[id] = new Game(50, 50);
+			games[id].join(ws);
+		}
+	});
+});
+
+var Game = function(cols, rows) {
+	this.initialize(cols, rows);
+	this.reset();
+};
+Game.prototype.initialize = function(cols, rows) {
+	this.cols = cols;
+	this.rows = rows;
+	this.players = {};
+	this.counter = 0;
+	this.board = [];
+};
+Game.prototype.reset = function() {
+	for (var y = 0; y < this.rows; y++) {
+		this.board[y] = [];
+		for (var x = 0; x < this.cols; x++) {
+			this.board[y][x] = 0;
+		}
+	}
+};
+Game.prototype.join = function(ws) {
+	var id = ++this.counter;
+	this.players[id] = {
+		ws: ws,
+		alive: true
+	};
+	ws.send(JSON.stringify({
+		type: 'newgame',
+		id: id,
+		board: this.board
+	}));
+	ws.on('message', function(json) {
+		var move = JSON.parse(json);
+		if (move.type == 'move') {
+			console.log(move);
+			if (this.valid(move)) {
+				this.board[move.y][move.x] = move.id;
+				for (var id in this.players)
+					this.players[id].ws.send(json);
+			} else {
+				move.type = 'gameover';
+				ws.send(JSON.stringify(move));
+				this.kill(move.id);
+			}
+		}
+	}.bind(this))
+	ws.on('close', function() {
+		console.log('Client disconnected');
+		delete this.players[id];
+	}.bind(this));
+};
+Game.prototype.kill = function(id) {
+	this.players[id].alive = false;
+	for (var id in this.players)
+		if (this.players[id].alive)
+			return;
+	this.reset();
+	for (var id in this.players) {
+		this.players[id].ws.send(JSON.stringify({
+			type: 'reset',
+			board: this.board
+		}));
+		this.players[id].alive = true;
+	}
+	console.log('reset');
+};
+Game.prototype.valid = function(move) {
+	if (move.x < 0 || this.cols <= move.x) return false;
+	if (move.y < 0 || this.rows <= move.y) return false;
+	if (this.board[move.y][move.x] != 0) return false;
+	if (!this.players[move.id].alive) return false;
+	return true;
+};
